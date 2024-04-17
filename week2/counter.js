38df1dd54ed5bb9e214ad6d515d21c3bbec5fbff
Ishitaa Dhingra <ishitaadhingra@gmail.com>
2024-04-18 02:06:06 +0530

    2-counter solution

--- week2/counter.js
+++ week2/counter.js
@@ -11,3 +11,15 @@ function updatecounter() {
   console.log(count);
 }
 setInterval(updatecounter, 1000);
+
+// ## Counter without setInterval
+
+// Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
+
+function updateCounter() {
+  count++;
+  console.log(count);
+  setTimeout(updateCounter, 1000);
+}
+
+updateCounter();
